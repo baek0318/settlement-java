f6c42c0c68d11e476fed92cb4beb755b0ee282e4
peachberry <peachberry0318@gmail.com>
2021-06-12 10:24:05 +0900

    feat(DB) : 데이터베이스에 order orderdetail 저장 메서드 추가

--- src/test/java/com/pair/settlement/dbutil/DatabaseInsert.java
+++ src/test/java/com/pair/settlement/dbutil/DatabaseInsert.java
@@ -1,5 +1,9 @@
 package com.pair.settlement.dbutil;
 
+import com.pair.settlement.order.OrderDetail;
+import com.pair.settlement.order.OrderStatus;
+import com.pair.settlement.order.OrderTable;
+import com.pair.settlement.order.PaymentMethod;
 import com.pair.settlement.owner.Account;
 import com.pair.settlement.owner.Owner;
 import com.pair.settlement.user.User;
@@ -8,6 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.time.LocalDateTime;
 
 @Component
 public class DatabaseInsert {
@@ -32,5 +37,30 @@ public class DatabaseInsert {
         Account account = new Account(owner, bank, bankAccount, accountHolder);
         entityManager.persist(account);
         return account.getId();
+    }
+
+    @Transactional
+    public OrderTable saveOrder(Long ownerId, int totalPrice, OrderStatus status, LocalDateTime createdAt) {
+        Owner owner = entityManager.find(Owner.class, ownerId);
+        OrderTable order = OrderTable.builder()
+                .owner(owner)
+                .totalPrice(totalPrice)
+                .status(status)
+                .createdAt(createdAt)
+                .build();
+        entityManager.persist(order);
+        return order;
+    }
+
+    @Transactional
+    public OrderDetail saveOrderDetail(Long orderId, PaymentMethod paymentMethod, int price) {
+        OrderTable order = entityManager.find(OrderTable.class, orderId);
+        OrderDetail orderDetail = OrderDetail.builder()
+                .orderTable(order)
+                .price(price)
+                .paymentMethod(paymentMethod)
+                .build();
+        entityManager.persist(orderDetail);
+        return orderDetail;
     }
 }
