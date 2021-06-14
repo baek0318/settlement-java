5fd3df87657d4847347897a8f33a1a5e6a3b85e6
peachberry <peachberry0318@gmail.com>
2021-06-15 00:31:19 +0900

    feat(Order) : OrderRepository 다이나믹 param 테스트

--- src/main/java/com/pair/settlement/order/OrderRepository.java
+++ src/main/java/com/pair/settlement/order/OrderRepository.java
@@ -2,5 +2,5 @@ package com.pair.settlement.order;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderRepository extends JpaRepository<OrderTable, Long> {
+public interface OrderRepository extends JpaRepository<OrderTable, Long>, OrderRepositoryCustom {
 }
