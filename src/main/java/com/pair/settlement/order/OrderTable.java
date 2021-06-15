f8994e5971b0ae818ac1796b5386e814b475225f
peachberry <peachberry0318@gmail.com>
2021-06-15 23:36:36 +0900

    feat(Order) : 일대다 맵핑 필드 추가

--- src/main/java/com/pair/settlement/order/OrderTable.java
+++ src/main/java/com/pair/settlement/order/OrderTable.java
@@ -9,6 +9,7 @@ import org.hibernate.annotations.GeneratorType;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Getter
@@ -30,6 +31,9 @@ public class OrderTable {
 
     private LocalDateTime createdAt;
 
+    @OneToMany(mappedBy = "orderTable")
+    private List<OrderDetail> orderDetails;
+
     @Builder
     public OrderTable(Owner owner, int totalPrice, OrderStatus status, LocalDateTime createdAt) {
         this.owner = owner;
@@ -40,5 +44,9 @@ public class OrderTable {
 
     public void setOwner(Owner owner) {
         this.owner = owner;
+    }
+
+    public void setOrderDetail(OrderDetail orderDetail) {
+        orderDetails.add(orderDetail);
     }
 }
