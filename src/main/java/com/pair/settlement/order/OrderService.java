176eef0ae075dd7969df4990feea6ce77928f335
peachberry <peachberry0318@gmail.com>
2021-06-15 23:24:27 +0900

    refactor (Order) : Order에서 담당하던 detail 저장을 OrderDetailService로 위임

--- src/main/java/com/pair/settlement/order/OrderService.java
+++ src/main/java/com/pair/settlement/order/OrderService.java
@@ -1,9 +1,11 @@
 package com.pair.settlement.order;
 
 import com.pair.settlement.order.dto.OrderDetailSaveRequest;
+import com.pair.settlement.order.dto.OrderInfoResponse;
 import com.pair.settlement.order.dto.OrderSaveResponse;
 import com.pair.settlement.owner.Owner;
 import com.pair.settlement.owner.OwnerService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -13,12 +15,12 @@ import java.util.List;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    private final OrderDetailRepository orderDetailRepository;
+    private final OrderDetailService orderDetailService;
     private final OwnerService ownerService;
 
-    public OrderService(OrderRepository orderRepository, OrderDetailRepository orderDetailRepository, OwnerService ownerService) {
+    public OrderService(OrderRepository orderRepository, OrderDetailService orderDetailService, OwnerService ownerService) {
         this.orderRepository = orderRepository;
-        this.orderDetailRepository = orderDetailRepository;
+        this.orderDetailService = orderDetailService;
         this.ownerService = ownerService;
     }
 
@@ -26,12 +28,10 @@ public class OrderService {
         Owner owner = ownerService.findOwner(ownerId);
         order.setOwner(owner);
         OrderTable orderTable = orderRepository.save(order);
-        List<Long> detailsId = new ArrayList<>();
-        for(OrderDetail detail : details) {
-            detail.setOrder(orderTable);
-            detailsId.add(orderDetailRepository.save(detail).getId());
-        }
-        return new OrderSaveResponse(orderTable.getId(), detailsId);
+        return new OrderSaveResponse(
+                orderTable.getId(),
+                orderDetailService.saveDetails(details, orderTable)
+        );
     }
 
     public List<OrderTable> findInfo(String ownerId, String orderId, String fromDateTime, String toDateTime) {
@@ -46,5 +46,10 @@ public class OrderService {
                 && (toDateTime == null || toDateTime.isEmpty())){
             throw new RuntimeException("모든 param이 null이여서는 안됩니다");
         }
+    }
+
+    public ResponseEntity<OrderInfoResponse> findInfoWithDetail(Long orderId) {
+
+        return null;
     }
 }
---
+++ src/test/java/com/pair/settlement/order/detail/OrderDetailServiceTest.java
@@ -0,0 +1,57 @@
+package com.pair.settlement.order.detail;
+
+import com.pair.settlement.order.*;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class OrderDetailServiceTest {
+
+    @Mock
+    OrderDetailRepository orderDetailRepository;
+
+    @InjectMocks
+    OrderDetailService orderDetailService;
+
+    @Test
+    void 주문_인스턴스가_비어있는_경우() {
+        OrderDetail orderDetail = OrderDetail.builder()
+                .paymentMethod(PaymentMethod.CARD)
+                .price(2000)
+                .build();
+        List<OrderDetail> list = Arrays.asList(orderDetail);
+        Assertions.assertThatThrownBy(
+                () -> orderDetailService.saveDetails(list, null))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 총_주문_금액이_일치하지_않는경우() {
+        OrderTable order = OrderTable.builder()
+                .totalPrice(6000)
+                .build();
+        OrderDetail orderDetail = OrderDetail.builder()
+                .paymentMethod(PaymentMethod.CARD)
+                .price(2000)
+                .orderTable(order)
+                .build();
+        OrderDetail orderDetail2 = OrderDetail.builder()
+                .paymentMethod(PaymentMethod.CASH)
+                .price(3000)
+                .orderTable(order)
+                .build();
+        List<OrderDetail> list = Arrays.asList(orderDetail, orderDetail2);
+        int totalPrice = 6000;
+        Assertions.assertThatThrownBy(() -> orderDetailService.saveDetails(list, order))
+                .isInstanceOf(RuntimeException.class);
+    }
+}
