16f3ddf2fb099bb13a8b6ec83c47a7ead45d16ab
peachberry <peachberry0318@gmail.com>
2021-06-25 23:15:20 +0900

    feat(Settle) : Settle 도메인 생성

--- repository/src/main/generated/com/pair/settle/QSettle.java
+++ repository/src/main/generated/com/pair/settle/QSettle.java
@@ -7,6 +7,7 @@ import com.querydsl.core.types.dsl.*;
 import com.querydsl.core.types.PathMetadata;
 import javax.annotation.Generated;
 import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
 
 
 /**
@@ -17,6 +18,8 @@ public class QSettle extends EntityPathBase<Settle> {
 
     private static final long serialVersionUID = -1283873561L;
 
+    private static final PathInits INITS = PathInits.DIRECT2;
+
     public static final QSettle settle = new QSettle("settle");
 
     public final NumberPath<Integer> amount = createNumber("amount", Integer.class);
@@ -25,18 +28,27 @@ public class QSettle extends EntityPathBase<Settle> {
 
     public final NumberPath<Long> id = createNumber("id", Long.class);
 
-    public final NumberPath<Long> ownerId = createNumber("ownerId", Long.class);
+    public final com.pair.owner.QOwner owner;
 
     public QSettle(String variable) {
-        super(Settle.class, forVariable(variable));
+        this(Settle.class, forVariable(variable), INITS);
     }
 
     public QSettle(Path<? extends Settle> path) {
-        super(path.getType(), path.getMetadata());
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
     }
 
     public QSettle(PathMetadata metadata) {
-        super(Settle.class, metadata);
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QSettle(PathMetadata metadata, PathInits inits) {
+        this(Settle.class, metadata, inits);
+    }
+
+    public QSettle(Class<? extends Settle> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.owner = inits.isInitialized("owner") ? new com.pair.owner.QOwner(forProperty("owner")) : null;
     }
 
 }
